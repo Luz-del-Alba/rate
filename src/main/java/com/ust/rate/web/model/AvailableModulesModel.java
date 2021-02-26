3df2fca869ae1128507c80d56eace496b2598b5f
Cristhian Castaneda <ccastanedarivera@gmail.com>
2021-02-26 06:47:57 -0600

    create evaluation service

--- src/main/java/com/ust/rate/web/model/AvailableModulesModel.java
+++ src/main/java/com/ust/rate/web/model/AvailableModulesModel.java
@@ -3,10 +3,8 @@ package com.ust.rate.web.model;
 import com.ust.rate.domain.entity.type.Range;
 import lombok.Data;
 
-import java.math.BigDecimal;
-
 @Data
 public class AvailableModulesModel {
     private String _id;
-    private Range<BigDecimal> details;
+    private Range<String> details;
 }
