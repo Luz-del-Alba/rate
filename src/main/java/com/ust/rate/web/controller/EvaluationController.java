12002354d93f5d463fcca4882055bfc7d124b6a4
Cristhian Castaneda <ccastanedarivera@gmail.com>
2021-02-26 15:17:38 -0600

    Fix CORS

--- src/main/java/com/ust/rate/web/controller/EvaluationController.java
+++ src/main/java/com/ust/rate/web/controller/EvaluationController.java
@@ -3,7 +3,7 @@ package com.ust.rate.web.controller;
 import com.ust.rate.service.EvaluationService;
 import com.ust.rate.web.model.EvaluationModel;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +14,7 @@ public class EvaluationController {
 
     private final EvaluationService evaluationService;
 
-    @PostMapping
+    @GetMapping
     public EvaluationModel evaluate() {
         return evaluationService.evaluate();
     }
